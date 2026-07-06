0fa8a22be102fe53b7d43afb4204c5d10bc7539c
Alexandra Prokhina <alexandraprokhina@MacBook-Air-Alexandra.local>
2026-07-06 17:45:35 +0200

    message

--- etl/load_weather.py
+++ etl/load_weather.py
@@ -1,1 +1,26 @@
+import pandas as pd
+from sqlalchemy import create_engine
 
+
+def load_weather(file_path):
+    """
+    Load transformed weather data into PostgreSQL
+    """
+
+    df = pd.read_csv(file_path)
+
+    # подключение к БД 
+    engine = create_engine("postgresql://user:password@localhost:5432/weather_db")
+
+    df.to_sql(
+        "weather_daily",
+        engine,
+        if_exists="replace",
+        index=False
+    )
+
+    print("Data loaded into PostgreSQL")
+
+
+if __name__ == "__main__":
+    load_weather("data/processed/weather_daily.csv")
