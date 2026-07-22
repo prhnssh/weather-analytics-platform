c23ed7e1af69980cc1efe1207c1cb0949d70ac8f
Alexandra Prokhina <alexandraprokhina@MacBook-Air-Alexandra.local>
2026-07-22 11:34:03 +0200

    Добавлены поля месяца в витрину дашборда

--- sql/create_monthly_dashboard_mart.sql
+++ sql/create_monthly_dashboard_mart.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS mart;
-
 
 DROP TABLE IF EXISTS mart.weather_monthly_dashboard;
 
@@ -9,6 +7,22 @@ CREATE TABLE mart.weather_monthly_dashboard AS
 SELECT
     city,
     DATE_TRUNC('month', date::date)::date AS month,
+    EXTRACT(MONTH FROM date)::int AS month_number,
+
+    CASE
+        WHEN EXTRACT(MONTH FROM date) = 1 THEN 'Январь'
+        WHEN EXTRACT(MONTH FROM date) = 2 THEN 'Февраль'
+        WHEN EXTRACT(MONTH FROM date) = 3 THEN 'Март'
+        WHEN EXTRACT(MONTH FROM date) = 4 THEN 'Апрель'
+        WHEN EXTRACT(MONTH FROM date) = 5 THEN 'Май'
+        WHEN EXTRACT(MONTH FROM date) = 6 THEN 'Июнь'
+        WHEN EXTRACT(MONTH FROM date) = 7 THEN 'Июль'
+        WHEN EXTRACT(MONTH FROM date) = 8 THEN 'Август'
+        WHEN EXTRACT(MONTH FROM date) = 9 THEN 'Сентябрь'
+        WHEN EXTRACT(MONTH FROM date) = 10 THEN 'Октябрь'
+        WHEN EXTRACT(MONTH FROM date) = 11 THEN 'Ноябрь'
+        WHEN EXTRACT(MONTH FROM date) = 12 THEN 'Декабрь'
+    END AS month_name,
 
     ROUND(AVG(avg_temp)::numeric, 2) AS avg_temp,
     ROUND(MAX(max_temp)::numeric, 2) AS max_temp,
@@ -21,5 +35,6 @@ FROM weather.weather_daily
 
 GROUP BY
     city,
-    DATE_TRUNC('month', date::date)::date;
+    DATE_TRUNC('month', date::date)::date,
+    EXTRACT(MONTH FROM date);
 
